09e23b0ad9b35c1e904e0ff7fcb58982329494f4
Amit Salvi <amit.salvi@outlook.com>
2022-09-26 13:30:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from replit import audio
+import logging
+
+def main():
+    source = audio.play_file('song.mp3')
+    source.toggle_playing()
+
+    if source.get_paused() == True:
+      source.toggle_playing()
+
+    volume = 1
+    loops = 0
+
+    print('type "up" or "down" to change volume')
+    print('type "loop" to add another loop')
+    print('press enter to play/pause')
+
+    while True:
+        print(
+            f'volume is at {source.volume * 100}% with',
+            f'{source.loops_remaining} loops remaining.'
+        )
+        
+        cmd = input('> ').lower()
+        
+        if cmd == 'play':
+            print(source.get_remaining())
+        elif cmd == 'pause':
+            print(source.get_paused())
+
+
+main()
